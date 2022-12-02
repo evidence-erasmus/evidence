844412fd4143b27b6fe172f76c439875a35bf72c
Ooker <maaker@gmail.com>
2022-12-02 17:23:15 +0200

    adding some content

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <h1>About us page</h1>
+        <h1>{{$t('about.title')}}</h1>
+
+        <p>{{$t('about.01')}}</p>
     </div>
 </template>
 
 <script setup>
+    import { useI18n } from 'vue-i18n';
+    const { t } = useI18n({ useScope: 'global' });
     definePageMeta({
         layout: "content",
     });
+
     // defineI18nRoute({
     //     paths: {
     //         en: '/about-us', // -> accessible at /about-us (no prefix since it's the default locale)
