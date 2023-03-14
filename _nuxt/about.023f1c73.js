c1bed33819c3d2504afdb73fa056439aa4ad2f23
ooker <maaker+github@gmail.com>
2023-03-14 22:18:23 +0200

    Updates

---
+++ _nuxt/about.023f1c73.js
@@ -0,0 +1,1 @@
+import{s as o,o as l,l as s,v as t,t as a}from"./entry.27bec4c0.js";const p={__name:"about",setup(n){return o({useScope:"global"}),(e,u)=>(l(),s("div",null,[t("h1",null,a(e.$t("about.title")),1),t("p",null,a(e.$t("about.01")),1)]))}};export{p as default};
