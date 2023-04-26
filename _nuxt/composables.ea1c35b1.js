ef78cfca2319bcab71ffc68b49030dfc44f98e78
ooker <maaker+github@gmail.com>
2023-04-26 23:48:02 +0300

    Updates

---
+++ _nuxt/composables.ea1c35b1.js
@@ -0,0 +1,1 @@
+import{a1 as a,N as u,z as n,a2 as r,a3 as c,a4 as i}from"./entry.11c91e78.js";function p(t={}){const o=t.i18n||a(u().$i18n),e=n(),s=r();return c({router:s,route:e,i18n:o})}function l(t={}){const o=t.i18n||a(u().$i18n),e=n(),s=r();return i({router:s,route:e,i18n:o})}export{l as a,p as u};
--- _nuxt/config.c5a86849.js
+++
@@ -1,1 +0,0 @@
-import{$ as p,N as o,a0 as i}from"./entry.03850fa9.js";const a={},f=p(a);function s(){const n=o();return n._appConfig||(n._appConfig=i(f)),n._appConfig}export{s as u};
