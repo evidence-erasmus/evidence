c130007ac2e3e45c36d4aaf9aae37de0095de303
Ooker <maaker@gmail.com>
2023-05-23 19:50:33 +0300

    et

---
+++ pages/climate-change/student.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <template v-if="locale === 'et'">
+            <ModulesVirusesStudentEn />
+        </template>
+        <template v-else>
+            <ModulesVirusesStudentEn />
+        </template>
+    </div>
+</template>
+
+<script setup>
+    import { useI18n } from 'vue-i18n';
+    const { locale, t } = useI18n({ useScope: 'global' });
+    definePageMeta({
+        layout: "content-left",
+    });
+
+
+</script>
