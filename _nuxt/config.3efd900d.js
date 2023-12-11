c4dba52dee700ba8961f572586dbb0702431c235
Ooker <maaker@gmail.com>
2023-12-11 23:19:33 +0200

    Updates

---
+++ _nuxt/config.3efd900d.js
@@ -0,0 +1,1 @@
+import{V as p,W as o,E as i}from"./entry.f32193ec.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
--- _nuxt/config.71caf679.js
+++
@@ -1,1 +0,0 @@
-import{V as p,W as o,E as i}from"./entry.d94c763c.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
