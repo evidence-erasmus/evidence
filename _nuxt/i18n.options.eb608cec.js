9a67a1f3b410015770bee16d4ef5e4b6dd6a5080
Ooker <maaker@gmail.com>
2024-01-28 19:57:20 +0200

    Updates

--- _nuxt/i18n.options.eb608cec.js
+++
@@ -1,1 +0,0 @@
-import{ac as o,ad as i,ae as n}from"./entry.59a046a0.js";async function s(){const t=await o(()=>import("./i18n.config.d13c11df.js"),[],import.meta.url).then(a=>a.default||a);return i(t)?await t():n(t)?t:{}}export{s as default};
