6debef74bec75576687ecc3eb07635235b78c7ec
Ooker <maaker@gmail.com>
2024-08-30 11:12:16 +0300

    Updates

---
+++ _nuxt/config.123b699d.js
@@ -0,0 +1,1 @@
+import{a0 as p,a1 as o,E as i}from"./entry.6754a314.js";const a={},f=p(a);function s(){const n=i();return n._appConfig||(n._appConfig=o(f)),n._appConfig}export{s as u};
---
+++ _nuxt/useStudentEvolutionGuidedMyth6.3c0640cf.js
@@ -0,0 +1,1 @@
+import{x as a,y as s}from"./entry.6754a314.js";const o=a("studentEvolutionGuidedMyth6",{state:()=>({q1:"",q2:"",q3:"",q4:"",reliability:[["","","'0'",""],["","","'0'",""],["","","'0'",""]]}),actions:{addReliabilityRow(){this.reliability.push(["","","'0'",""])},changeReliabilityValue(t,i,e){this.reliability[t][i]=e}},persist:{storage:s.localStorage}});export{o as u};
