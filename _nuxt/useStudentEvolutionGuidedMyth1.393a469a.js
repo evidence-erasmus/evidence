7cf9c715d5826cbf204a6907f4d61d29832ee83a
Ooker <maaker@gmail.com>
2024-11-20 11:11:57 +0200

    Updates

---
+++ _nuxt/useStudentEvolutionGuidedMyth1.393a469a.js
@@ -0,0 +1,1 @@
+import{z as a,A as s}from"./entry.cc648abe.js";const o=a("studentEvolutionGuidedMyth1",{state:()=>({q1:"",q2:"",q3:"",q4:"",q5:"",reliability:[["","","'0'",""],["","","'0'",""],["","","'0'",""]]}),actions:{addReliabilityRow(){this.reliability.push(["","","'0'",""])},changeReliabilityValue(t,i,e){this.reliability[t][i]=e}},persist:{storage:s.localStorage}});export{o as u};
---
+++ _nuxt/useUserPreferences.5307ba83.js
@@ -0,0 +1,1 @@
+import{z as s}from"./entry.cc648abe.js";const a=s("userPreferences",{state:()=>({tocOpen:!0,modalOpen:!1,modalName:"",learnPathViruses:0}),actions:{toggleToC(){this.tocOpen=!this.tocOpen},toggleModal(e){this.modalName=e,this.modalOpen=!this.modalOpen,console.log("MODAL",this.modalName)},setLearnPath(e,t){this[e]=t}},persist:!0});export{a as u};
